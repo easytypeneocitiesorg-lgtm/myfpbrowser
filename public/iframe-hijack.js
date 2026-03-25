3cb33fc9e35a84f2bd8ec76f8f829cf03bbf32c4
easytypeneocitiesorg-lgtm <easytype.neocities.org@gmail.com>
2026-03-25 18:58:22 -0500

    Create iframe-hijack.js

---
+++ public/iframe-hijack.js
@@ -0,0 +1,21 @@
+// iframe-hijack.js
+document.addEventListener('click', e => {
+  const a = e.target.closest('a');
+  if (a && a.href && !a.target) {
+    e.preventDefault();
+    window.parent.postMessage({type:'openNewTab', url:a.href}, '*');
+  }
+});
+
+document.addEventListener('submit', e => {
+  const f = e.target;
+  e.preventDefault();
+  const action = f.action || window.location.href;
+  window.parent.postMessage({type:'openNewTab', url:action}, '*');
+});
+
+const origAssign = window.location.assign;
+window.location.assign = function(url){ window.parent.postMessage({type:'openNewTab', url:url}, '*'); };
+
+const origReplace = window.location.replace;
+window.location.replace = function(url){ window.parent.postMessage({type:'openNewTab', url:url}, '*'); };
